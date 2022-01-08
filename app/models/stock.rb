9fb6373a6307b02fa27cd2689470dc0a7c5c8501
megharosejayan <megharose15@gmail.com>
2022-01-09 00:00:24 +0530

    Stock listing view

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,7 +1,11 @@
 class Stock < ApplicationRecord
+    has_many :user_stocks
+    has_many :users, through: :user_stocks
+
+    validates :name, :ticker, presence: true
     def self.new_lookup(ticker_symbol)
-        client = IEX::Api::Client.new(publishable_token: #insert publishable_token,
-                                  secret_token: #insert secret_token,
+        client = IEX::Api::Client.new(publishable_token: #pupublishable_token,
+                                  secret_token: #secret_token,
                                   endpoint: 'https://sandbox.iexapis.com/v1')
         begin
             new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name , last_price: client.price(ticker_symbol))
