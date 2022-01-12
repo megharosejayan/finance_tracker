9fc4118b74567911f898b54c72849ccbb196925e
megharosejayan <megharose15@gmail.com>
2022-01-13 00:00:23 +0530

    Implement stock trading restrictions

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,8 +4,8 @@ class Stock < ApplicationRecord
 
     validates :name, :ticker, presence: true
     def self.new_lookup(ticker_symbol)
-        client = IEX::Api::Client.new(publishable_token: #publishable_token,
-                                  secret_token: #secret_token,
+        client = IEX::Api::Client.new(publishable_token: 'Tpk_e9957a6fe68c4fad9a4b0f2bab2c78fe',
+                                  secret_token: 'Tsk_ed87434f037a4dee88a8d31931a5dfde',
                                   endpoint: 'https://sandbox.iexapis.com/v1')
         begin
             new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name , last_price: client.price(ticker_symbol))
@@ -13,4 +13,9 @@ class Stock < ApplicationRecord
             return nil
         end
     end
+
+    def self.check_db(ticker_symbol)
+        where(ticker: ticker_symbol).first
+    end
+
 end
